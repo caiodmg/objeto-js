1fc2ef887e243eded4331b6e1b4588f243958a0c
CAIO DOMINGOS <115330991+caiodmg@users.noreply.github.com>
2023-02-19 15:04:35 -0300

    Add files via upload

---
+++ objeto js/aula1/objeto.js
@@ -0,0 +1,10 @@
+const listaCPFs = [1111111, 22222, 3333];
+
+const informacoesPessoa = ["nome", "Jose", "idade", 32, "CPF", "11122233300"]
+
+console.log(informacoesPessoa[1]);
+
+const objetoPessoa = {
+    nome: "Jose",
+    idade: 32,
+};
